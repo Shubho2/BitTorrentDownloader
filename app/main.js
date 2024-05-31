a0b39c61cfadd5fca3e371d6338c5cf860b1cd47
Shubho Banerjee <shubho.banerjee@sap.com>
2024-05-31 21:56:43 +0530

    stage 2

--- app/main.js
+++ app/main.js
@@ -1,35 +1,10 @@
-const process = require("process");
-const util = require("util");
+const commandHandler = require("../lib/CommandHandler");
 
-// Examples:
-// - decodeBencode("5:hello") -> "hello"
-// - decodeBencode("10:hello12345") -> "hello12345"
-function decodeBencode(bencodedValue) {
-  // Check if the first character is a digit
-  if (!isNaN(bencodedValue[0])) {
-    const firstColonIndex = bencodedValue.indexOf(":");
-    if (firstColonIndex === -1) {
-      throw new Error("Invalid encoded value");
-    }
-    return bencodedValue.substr(firstColonIndex + 1);
-  } else {
-    throw new Error("Only strings are supported at the moment");
-  }
+const command = process.argv[2];
+
+if (command === "decode") {
+  commandHandler.decode(process.argv[3]);
+} else {
+  throw new Error(`Unknown command ${command}`);
 }
 
-function main() {
-  const command = process.argv[2];
-
-  // Uncomment this block to pass the first stage
-  if (command === "decode") {
-    const bencodedValue = process.argv[3];
-  
-    // In JavaScript, there's no need to manually convert bytes to string for printing
-    // because JS doesn't distinguish between bytes and strings in the same way Python does.
-    console.log(JSON.stringify(decodeBencode(bencodedValue)));
-  } else {
-    throw new Error(`Unknown command ${command}`);
-  }
-}
-
-main();
