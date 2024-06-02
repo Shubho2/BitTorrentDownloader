f44030e3acf7164c137105216e367142742fac4a
Shubho Banerjee <shubho.banerjee@sap.com>
2024-06-02 20:15:26 +0530

    stage 5

--- app/main.js
+++ app/main.js
@@ -4,6 +4,8 @@ const command = process.argv[2];
 
 if (command === "decode") {
   commandHandler.decodeCommand(process.argv[3]);
+} else if (command === "info") {
+  commandHandler.infoCommand(process.argv[3]);
 } else {
   throw new Error(`Unknown command ${command}`);
 }
