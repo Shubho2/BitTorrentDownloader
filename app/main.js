3a5157abcedee51ad4ada982be95140dac686d87
Shubho Banerjee <shubho.banerjee@sap.com>
2024-06-06 23:24:24 +0530

    stage 8

--- app/main.js
+++ app/main.js
@@ -1,11 +1,13 @@
-const commandHandler = require("../lib/CommandHandler");
+const CommandHandler = require("../lib/CommandHandler");
 
 const command = process.argv[2];
 
 if (command === "decode") {
-  commandHandler.decodeCommand(process.argv[3]);
+  CommandHandler.decodeCommand(process.argv[3]);
 } else if (command === "info") {
-  commandHandler.infoCommand(process.argv[3]);
+  CommandHandler.infoCommand(process.argv[3]);
+} else if (command === "peers") {
+  CommandHandler.peersCommand(process.argv[3]);
 } else {
   throw new Error(`Unknown command ${command}`);
 }
